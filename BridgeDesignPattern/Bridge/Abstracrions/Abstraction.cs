32c8065039fe8aeb63d924a8c6eb6198236d81a1
USER <USER@DESKTOP-20DRNS1>
2022-12-29 18:39:17 +0330

    add program

---
+++ BridgeDesignPattern/Bridge/Abstracrions/Abstraction.cs
@@ -0,0 +1,28 @@
+﻿using BridgeDesignPattern.Bridge.Implementors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDesignPattern.Bridge.Abstracrions
+{
+    public abstract class Abstraction
+    {
+        private readonly Implementor implementors;
+        public Abstraction()
+        {
+            this.implementors = new ConcreteImplementor();
+        }
+
+        public virtual void Function()
+        {
+            implementors.Implementation();
+        }
+    }
+
+    public class RefinedAAbstraction : Abstraction
+    { 
+    
+    }
+}
--- BridgeDesignPattern/Program.cs
+++ BridgeDesignPattern/Program.cs
@@ -1,1 +1,8 @@
-﻿ Console.WriteLine("Hello, World!");
+﻿using BridgeDesignPattern.Bridge.Abstracrions;
+
+Console.WriteLine("Hello, World!");
+Abstraction abstraction=new RefinedAAbstraction();
+abstraction.Function();
+ 
+Console.ReadKey();
+
